12dddebfa23e3e9aee8a39346d3523767988c18d
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-11-27 10:16:27 +0100

    Exercice 1 done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MethodExercicesFirstBattery
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const string NumberInputMsg = "Give me a number";
+            const string ErrorMsg = "Error, you must put an integer natural number (> 0)";
+            const int MinValue = 2;
+            const int MaxValue = 10;
+
+            int num = 0;
+            bool numVal = false;
+
+            while (!numVal)
+            {
+                Console.WriteLine(NumberInputMsg);
+                try
+                {
+                    num = Int32.Parse(Console.ReadLine());
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine(ErrorMsg);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(ErrorMsg);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(ErrorMsg);
+                }
+                if (num > 0)
+                {
+                    numVal = true;
+                }
+                else
+                {
+                    Console.WriteLine(ErrorMsg);
+                }
+            }
+            Console.WriteLine(ValidateNumberInRange(num, MinValue, MaxValue));
+
+        }
+        public static bool ValidateNumberInRange(int numV, int min, int max)
+        {
+            return numV >= min && numV <= max;
+        }
+    }
+}
