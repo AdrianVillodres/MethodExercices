8a190d7a9f9d0addcf23d37e2d4af0043be11ec6
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-11-27 10:44:41 +0100

    Exercice 2 done

--- Program.cs
+++ Program.cs
@@ -4,20 +4,22 @@
     {
         public static void Main()
         {
-            const string NumberInputMsg = "Give me a number";
-            const string ErrorMsg = "Error, you must put an integer natural number (> 0)";
-            const int MinValue = 2;
-            const int MaxValue = 10;
+            const string NumberInputMsg = "Give me a temperature";
+            const string OperationMsg = "Which conversion do you want to do: 1: Celsius to Fahrenhait, 2: Fahrenhait to Celsius, 3: Celsius to Kelvin";
+            const string ErrorMsg = "Error, you must put a float number (> 0)";
 
+            float temp = 0;
             int num = 0;
+            int tries = 3;
+            bool tempVal = false;
             bool numVal = false;
 
-            while (!numVal)
+            while (!tempVal)
             {
                 Console.WriteLine(NumberInputMsg);
                 try
                 {
-                    num = Int32.Parse(Console.ReadLine());
+                    temp = float.Parse(Console.ReadLine());
                 }
                 catch (OverflowException)
                 {
@@ -31,21 +33,73 @@
                 {
                     Console.WriteLine(ErrorMsg);
                 }
-                if (num > 0)
+                if (temp > 0f)
                 {
-                    numVal = true;
+                    tempVal = true;
                 }
                 else
                 {
                     Console.WriteLine(ErrorMsg);
                 }
             }
-            Console.WriteLine(ValidateNumberInRange(num, MinValue, MaxValue));
+            while (!numVal && tries > 0)
+            {
+                Console.WriteLine(OperationMsg);
+                try
+                {
+                    num = Int32.Parse(Console.ReadLine());
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine(ErrorMsg);
+                    tries--;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(ErrorMsg);
+                    tries--;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(ErrorMsg);
+                    tries--;
+                }
+                if (num > 0 && num <= 3)
+                {
+                    numVal = true;
+                }
+                else
+                {
+                    Console.WriteLine(ErrorMsg);
+                    tries--;
+                }
+            }
+
+            if(numVal && tempVal)
+            {
+                Console.WriteLine(CalculateTemp(temp, ref num));
+            }
+
 
         }
-        public static bool ValidateNumberInRange(int numV, int min, int max)
+
+        public static float CalculateTemp(float tempI, ref int op)
         {
-            return numV >= min && numV <= max;
+            float tempConversed = 0f;
+            switch (op)
+            {
+                case 1:
+                    tempConversed = tempI * 9 / 5 + 32;
+                    break;
+                case 2:
+                    tempConversed = (tempI - 32) * 5 / 9;
+                    break;
+                case 3:
+                    tempConversed = tempI + 273.15f;
+                    break;
+            }
+            return (float)Math.Round(tempConversed, 2);
         }
+        
     }
 }
