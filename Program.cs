fcb0d87a32948cdd6698559588fd9c18b8146dca
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-12-02 11:41:32 +0100

    fix: methods validate and factorize added

--- Program.cs
+++ Program.cs
@@ -16,28 +16,11 @@
             int[] expo = new int[maxFact];
             int index = 0;
             bool numValid = false;
-            
-
 
             while (!numValid)
             {
-                Console.WriteLine(IntroMsg);
-                try
-                {
-                    num = Int32.Parse(Console.ReadLine());
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine(ErrorMsg);
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine(ErrorMsg);
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine(ErrorMsg);
-                }
+                num = Validate(IntroMsg, ErrorMsg);
+
                 if (num > 0f)
                 {
                     numValid = true;
@@ -50,40 +33,68 @@
                 if (numValid)
                 {
                     og = num;
-                    
-                    for (int i = 2; i * i <= num; i++)
-                    {
-                        count = 0;
-                        while (num % i == 0)
-                        {
-                            num /= i;
-                            count++;
-                        }
-                        if(count > 0)
-                        {
-                            primes[index] = i;
-                            expo[index] = count;
-                            index++;
-                        }
-                    }
-                    if (num > 1)
-                    {
-                        primes[index] = num;
-                        expo[index] = 1;
-                        index++;
-                    }
+                    Factorize(num, primes, expo, ref index);
 
                     Console.Write($"{og} = ");
                     for (int i = 0; i < index; i++)
                     {
                         Console.Write($"{primes[i]}^{expo[i]} ");
                     }
-                    {
-
-                    }
                 }
             }
         }
 
+ 
+        public static int Validate(string introMsg, string errorMsg)
+        {
+            Console.WriteLine(introMsg);
+
+            try
+            {
+                return Int32.Parse(Console.ReadLine());
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(errorMsg);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(errorMsg);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(errorMsg);
+            }
+
+            return 0;
+        }
+
+        public static void Factorize(int num, int[] primes, int[] expo, ref int index)
+        {
+            int count;
+
+            for (int i = 2; i * i <= num; i++)
+            {
+                count = 0;
+                while (num % i == 0)
+                {
+                    num /= i;
+                    count++;
+                }
+                if (count > 0)
+                {
+                    primes[index] = i;
+                    expo[index] = count;
+                    index++;
+                }
+            }
+
+            if (num > 1)
+            {
+                primes[index] = num;
+                expo[index] = 1;
+                index++;
+            }
+        }
     }
 }
