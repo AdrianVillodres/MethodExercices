544cadf4d3d7c98cd7f2d84f779278eae7a8045e
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-12-02 12:27:56 +0100

    Feat: user gives a dni or nie and the program validates if the document is valid, added lines to visualize the parameters

--- Program.cs
+++ Program.cs
@@ -1,57 +1,76 @@
-﻿namespace MethodExercicesFirstBattery
+﻿using System;
+
+namespace MethodExercicesFirstBattery
 {
     public class Program
     {
         public static void Main()
         {
-            const string IntroMsg = "Give me a natural number";
-            const string ErrorMsg = "Error, you must put an integer number (> 0)";
+            const string InputMsg = "Which document you have: 1.DNI 2.NIE";
+            const string IntroDniMsg = "Give me your dni";
+            const string IntroNieMsg = "Give me your nie";
+            const string ValidDocMsg = "Your document is valid";
+            const string InvalidDocMsg = "Your document is invalid";            
+            const string ErrorMsg = "Error, you must put an integer number (> 0 / < 3)";
 
+            string doc;
             int num = 0;
-            int og = 0;
-            int numDiv = 1;
-            int count;
-            int maxFact = 32;
-            int[] primes = new int[maxFact];
-            int[] expo = new int[maxFact];
-            int index = 0;
             bool numValid = false;
+            bool docValid = false;
 
             while (!numValid)
             {
-                num = Validate(IntroMsg, ErrorMsg);
-
-                if (num > 0f)
+                numValid = Validate(ref num, InputMsg, ErrorMsg);
+            }
+            if (numValid)
+            {
+                switch (num)
                 {
-                    numValid = true;
-                }
-                else
-                {
-                    Console.WriteLine(ErrorMsg);
-                }
-
-                if (numValid)
-                {
-                    og = num;
-                    Factorize(num, primes, expo, ref index);
-
-                    Console.Write($"{og} = ");
-                    for (int i = 0; i < index; i++)
-                    {
-                        Console.Write($"{primes[i]}^{expo[i]} ");
-                    }
+                    case 1:
+                        Console.WriteLine(IntroDniMsg);
+                        doc = Console.ReadLine();
+                        docValid = ValidateFormatDni(doc);
+                        if (docValid)
+                        {
+                            Console.WriteLine(ValidDocMsg);
+                        }
+                        else
+                        {
+                            Console.WriteLine(InvalidDocMsg);
+                        }
+                            break;
+                    case 2:
+                        Console.WriteLine(IntroNieMsg);
+                        doc = Console.ReadLine();
+                        docValid = ValidateFormatNie(doc);
+                        if (docValid)
+                        {
+                            Console.WriteLine(ValidDocMsg);
+                        }
+                        else
+                        {
+                            Console.WriteLine(InvalidDocMsg);
+                        }
+                        break;
                 }
             }
         }
 
- 
-        public static int Validate(string introMsg, string errorMsg)
+        public static bool Validate(ref int num, string introMsg, string errorMsg)
         {
             Console.WriteLine(introMsg);
 
             try
             {
-                return Int32.Parse(Console.ReadLine());
+                num = int.Parse(Console.ReadLine());
+
+                if (num < 0)
+                {
+                    Console.WriteLine(errorMsg);
+                    return false;
+                }
+
+                return true;
             }
             catch (OverflowException)
             {
@@ -66,35 +85,48 @@
                 Console.WriteLine(errorMsg);
             }
 
-            return 0;
+            return false;
         }
 
-        public static void Factorize(int num, int[] primes, int[] expo, ref int index)
+
+
+        public static bool ValidateFormatDni(string document)
         {
-            int count;
+            int nums;
+            char letter;
+            char[] letters = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
 
-            for (int i = 2; i * i <= num; i++)
-            {
-                count = 0;
-                while (num % i == 0)
-                {
-                    num /= i;
-                    count++;
-                }
-                if (count > 0)
-                {
-                    primes[index] = i;
-                    expo[index] = count;
-                    index++;
-                }
-            }
 
-            if (num > 1)
-            {
-                primes[index] = num;
-                expo[index] = 1;
-                index++;
-            }
+            nums = int.Parse(document.Substring(0, 8));
+            letter = char.Parse(document.Substring(8));
+
+            //To visualize the parameters
+            /*Console.WriteLine(nums);
+            Console.WriteLine(letter);*/
+
+            return (char)letter == letters[nums % 23];
         }
+
+        public static bool ValidateFormatNie(string document)
+        {
+            int nums;
+            char letter;
+            char firstLetter;
+            char[] letters = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
+
+            firstLetter = char.Parse(document.Substring(0, 1));
+            nums = int.Parse(document.Substring(1, 7));
+            letter = char.Parse(document.Substring(8));
+           
+            //To visualize the parameters
+            /*Console.WriteLine(nums);
+            Console.WriteLine(letter);
+            Console.WriteLine(firstLetter);*/
+
+            return (char)letter == letters[nums % 23] && firstLetter == 'X' || firstLetter == 'Y' || firstLetter == 'Z';
+        }
+
+
+
     }
 }
